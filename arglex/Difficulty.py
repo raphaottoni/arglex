11be708f07675235d843569698375b8edb5e252d
Raphael Ottoni <rapha.ottoni@gmail.com>
2016-11-19 15:48:38 -0200

    adding generalization, inconsistency, intensifiers and bug fix on macros and difficulty

--- arglex/Difficulty.py
+++ arglex/Difficulty.py
@@ -7,23 +7,23 @@ class Difficulty:
   #constructor
   def __init__(self):
 
-    self.ngrams = [r''+macros._be + macros._intensadj1 + r'? easy',
-                   r''+macros._be + r' a ' + macros._intensadj1 + r'? breeze',
-                   r''+macros._be + r' a ' + macros._intensadj1 + r'? walk in the park',
-                   r''+macros._be + r' a ' + macros._intensadj1 + r'? piece of cake',
-                   r''+macros._be + r' a ' + macros._intensadj1 + r'? snap',
-                   r''+macros._be + r' a ' + macros._intensadj1 + r'? cinch',
+    self.ngrams = [r''+macros._be + r' ' +  macros._intensadj1 + r'? easy',
+                   r''+macros._be + r' a' + macros._intensadj1 + r'? breeze',
+                   r''+macros._be + r' a' + macros._intensadj1 + r'? walk in the park',
+                   r''+macros._be + r' a' + macros._intensadj1 + r'? piece of cake',
+                   r''+macros._be + r' a' + macros._intensadj1 + r'? snap',
+                   r''+macros._be + r' a' + macros._intensadj1 + r'? cinch',
                    r''+macros._be + r' ' +   macros._intensadj1 + r'? child\'s play',
                    r''+macros._be + r' ' +   macros._intensadj1 + r'? difficult',
-                   r''+macros._be + r' a ' + macros._intensadj1 + r'? pain',
-                   r''+macros._be + r' a ' + macros._intensadj1 + r'? pain in the (butt|neck|ass)',
-                   r''+macros._be + r' a ' + macros._intensadj1 + r'? (bitch|bastard) to',
+                   r''+macros._be + r' a' + macros._intensadj1 + r'? pain',
+                   r''+macros._be + r' a' + macros._intensadj1 + r'? pain in the (butt|neck|ass)',
+                   r''+macros._be + r' a' + macros._intensadj1 + r'? (bitch|bastard) to',
                    r''+macros._be + r' no picnic',
                    r''+macros._be + r' ' +     macros._intensadj1 + r'? tricky',
                    r''+macros._be + r' ' +     macros._intensadj1 + r'? arduous',
-                   r''+macros._be + r' a ' +   macros._intensadj1 + r'? challenge',
-                   r''+macros._be + r' a ' +   macros._intensadj1 + r'? challenging',
-                   r''+macros._have + r' a ' + macros._intensadj1 + r'? (hard|difficult) time'
+                   r''+macros._be + r' a' +   macros._intensadj1 + r'? challenge',
+                   r''+macros._be + r' a' +   macros._intensadj1 + r'? challenging',
+                   r''+macros._have + r' a' + macros._intensadj1 + r'? (hard|difficult) time'
                   ]
 
 
